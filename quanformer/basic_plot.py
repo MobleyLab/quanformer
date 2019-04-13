d1058ced30e8c50ce2ae8bc932fe556ea759d2da
Victoria Lim <lim.t.victoria@gmail.com>
2019-04-12 18:24:35 -0700

    basic plotting for sdf files

---
+++ quanformer/basic_plot.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+"""
+basic_plot.py
+
+Basic plots (lines or bars) of information in SD tags.
+
+Version:    Apr 12 2019
+By:         Victoria T. Lim
+
+"""
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("Agg")  # for Mac OS X error of NSInvalidArgumentException
+
+import quanformer.proc_tags as pt
+import quanformer.reader as reader
+
+
+def basic_plot(infile, tag, style, take_relative=False, har_to_kcal=False):
+    """
+    TODO
+
+    Parameters
+    ----------
+    infile : string
+        Name of SDF file with information in SD tags.
+    tag : string
+
+    style : string
+        plot style. can be 'scatter', 'line', or 'bar'
+        TODO
+
+    """
+    # Open molecule file.
+    mols = reader.read_mols(infile)
+
+    for i, mol_i in enumerate(mols):
+
+        # get array of all conformer data of this mol
+        try:
+            data_array = np.fromiter(pt.get_sd_list(mol_i, datum='', taglabel=tag), dtype=np.float64)
+        except ValueError:
+            data_array = np.asarray([np.nan])*mol_i.NumConfs()
+
+        # exclude conformers for which job did not finish (nan)
+        nanIndices = np.argwhere(np.isnan(data_array))
+        for i in reversed(nanIndices):  # loop in reverse to delete correctly
+            data_array = np.delete(data_array, i)
+
+        # take relative values by subtracting minimum
+        if take_relative:
+            data_array = data_array - np.amin(data_array)
+
+        # generate plot
+        plt.plot(data_array)
+        plt.title(mol_i.GetTitle()+'\n'+tag, fontsize=14)
+        plt.savefig(f'output_{i}.png', bbox_inches='tight')
+        plt.show()
+
---
+++ quanformer/tests/test_basic_plot.py
@@ -0,0 +1,35 @@
+"""
+test_basic_plot.py
+"""
+import sys
+import os
+import pytest
+import shutil
+
+# define location of input files for testing
+mydir = os.path.dirname(os.path.abspath(__file__))
+
+# import functions to aid testing
+sys.path.append(os.path.join(os.path.dirname(__file__), 'helpers'))
+from helper import *
+
+#from quanformer.basic_plot import *
+
+# -----------------------
+
+
+def test_survey_energies_plot():
+    basic_plot(os.path.join(mydir, 'data_tests', 'survey_confs', 'divrefine-220.sdf'),
+    tag='QM Psi4 Final Opt. Energy (Har) b3lyp-d3mbj/def2-tzvp',
+    style='line',
+    take_relative=True,
+    har_to_kcal=True)
+    os.remove('output_0.png')
+    os.remove('output_1.png')
+
+
+# test manually without pytest
+if 1:
+    sys.path.insert(0, '/home/limvt/Documents/quanformer/quanformer')
+    from basic_plot import *
+    test_survey_energies_plot()
